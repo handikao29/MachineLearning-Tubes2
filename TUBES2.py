0cfa1f0aa075b0e60ae92a5938506a1428719861
famkampm <farhanaamiin@gmail.com>
2018-11-14 13:15:20 +0700

    feed forward until make an output

--- TUBES2.py
+++ TUBES2.py
@@ -1,57 +1,74 @@
-import random
+import numpy as np
 
-class NN:
 
-    def __init__(self, data, label, epoch, momentum, batch_size):
-        self.data = data
-        self.label = label
-        self.epoch = epoch
-        self.momentum = momentum
-        self.batch_size = batch_size
+def sigmoid(x):
+    return 1.0/(1+ np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input      = x
+        # self.weights1 = np.random.uniform(-.5,.5,[self.input.shape[1],4])
+        # self.weights1   = np.random.rand(self.input.shape[1],4)
+        # self.weights2   = np.random.rand(4,1)
+        self.y          = y
+        self.output     = np.zeros(self.y.shape)
         self.n_hidden_layer = 0
         self.hidden_layer = []
+        self.weight_hidden_layer = []
+        # self.weight_hidden_layer.append(self.weights1)
+        # self.weight_hidden_layer.append(self.weights2)
+
+
 
     def add(self,unit):
         if self.n_hidden_layer < 10:
             self.hidden_layer.append(unit)
             self.n_hidden_layer +=1
 
-    def fit(self, x, y):
-        self.array_of_node = []
-        self.num_node = 0
-        input_node = len(x[0])
-        input_array = []
-        for i in range(input_node):
-            input_array.append(i)
-            self.num_node += 1
-        self.node_per_layer = []
-        # self.index_per_layer =
-        self.index_per_layer = [[]]
-        self.index_per_layer.append(input_array)
-        self.index_per_layer.remove([])
+    def fit(self, batch_size, epoch):
+        self.batch_size = batch_size
+        self.epoch = epoch
+        print ("self.hidden_layer[0]", self.hidden_layer[0])
+        self.weights_input = np.random.uniform(-.5,.5,[self.input.shape[1],self.hidden_layer[0]])
+        # self.weight_hidden_layer.append(self.weights_input)
+        for i in range(len(self.hidden_layer)):
+            if i > 0:
+                weight_layer_temp = np.random.uniform(-.5,.5,[self.hidden_layer[i-1],self.hidden_layer[i]])
+                self.weight_hidden_layer.append(weight_layer_temp)
 
-        # add hidden layer
-        for n_node in self.hidden_layer:
-            # print ("n_node", n_node)
-            temp_array = []
-            temp_val = self.num_node
-            for i in range(n_node):
-                temp_array.append(i+self.num_node)
-                # self.num_node += 1
-                temp_val += 1
-            self.num_node = temp_val
-            self.index_per_layer.append(temp_array)
+        self.weight_output = np.random.uniform(-.5,.5,[self.hidden_layer[-1],1])
+        # self.weight_hidden_layer.append(self.weight_output)
 
-        # add output layer
-        output_array = [self.num_node]
-        self.index_per_layer.append(output_array)
+        print ("self.weight_hidden_layer\n", self.weight_hidden_layer)
 
-        # create adjacent matrix
-
-        self.adjacent_matrix = [[0] * self.num_node] * self.num_node
-        for row in range(self.num_node):
-            for column in range(row+1):
-                self.adjacent_matrix[row][column] = random.uniform(-0.05, 0.05)
+        self.feedforward()
 
 
-        print (self.adjacent_matrix)
+
+    def feedforward(self):
+        self.layer = []
+        for i in range(self.n_hidden_layer):
+            if i == 0:
+                self.layer.append(sigmoid(np.dot(self.input, self.weights_input)))
+            else:
+                self.layer.append(sigmoid(np.dot(self.layer[i-1], self.weight_hidden_layer[i-1])))
+
+
+
+        output_layer = sigmoid(np.dot(self.layer[-1], self.weight_output))
+        # self.layer.append(output_layer)
+        # self.output = sigmoid(np.dot(self.layer1, self.weights2))
+        self.output = output_layer
+
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
