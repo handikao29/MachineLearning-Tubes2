155f4a1bb492dcb779307636eeec835a1e59a464
famkampm <farhanaamiin@gmail.com>
2018-11-14 23:49:49 +0700

    edit backkprop

--- TUBES2.py
+++ TUBES2.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import deque
 
 
 def sigmoid(x):
@@ -9,18 +10,15 @@ def sigmoid_derivative(x):
 
 
 class NeuralNetwork:
-    def __init__(self, x, y):
+    def __init__(self, x, y, learning_rate, momentum):
         self.input      = x
-        # self.weights1 = np.random.uniform(-.5,.5,[self.input.shape[1],4])
-        # self.weights1   = np.random.rand(self.input.shape[1],4)
-        # self.weights2   = np.random.rand(4,1)
         self.y          = y
         self.output     = np.zeros(self.y.shape)
+        self.learning_rate = learning_rate
+        self.momentum = momentum
         self.n_hidden_layer = 0
         self.hidden_layer = []
         self.weight_hidden_layer = []
-        # self.weight_hidden_layer.append(self.weights1)
-        # self.weight_hidden_layer.append(self.weights2)
 
 
 
@@ -46,29 +44,48 @@ class NeuralNetwork:
         print ("self.weight_hidden_layer\n", self.weight_hidden_layer)
 
         self.feedforward()
-
+        self.backprop()
 
 
     def feedforward(self):
-        self.layer = []
+        self.hidden_layer_out = []
+        self.hidden_layer_in = []
         for i in range(self.n_hidden_layer):
             if i == 0:
-                self.layer.append(sigmoid(np.dot(self.input, self.weights_input)))
+                self.hidden_layer_in.append(np.dot(self.input, self.weights_input))
+                self.hidden_layer_out.append(sigmoid(self.hidden_layer_in[i]))
             else:
-                self.layer.append(sigmoid(np.dot(self.layer[i-1], self.weight_hidden_layer[i-1])))
+                self.hidden_layer_in.append(np.dot(self.hidden_layer_out[i-1], self.weight_hidden_layer[i-1]))
+                self.hidden_layer_out.append(sigmoid(self.hidden_layer_in[i]))
+
+        self.output_layer_in = np.dot(self.hidden_layer_out[-1], self.weight_output)
+        self.output_layer_out = sigmoid(self.output_layer_in)
+        self.output = self.output_layer_out
 
 
-
-        output_layer = sigmoid(np.dot(self.layer[-1], self.weight_output))
-        # self.layer.append(output_layer)
-        # self.output = sigmoid(np.dot(self.layer1, self.weights2))
-        self.output = output_layer
 
     def backprop(self):
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+        deq = deque([])
+        # deq.appendleft(1)
 
-        # update the weights with the derivative (slope) of the loss function
-        self.weights1 += d_weights1
-        self.weights2 += d_weights2
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        self.o1 = (self.y - self.output) * sigmoid_derivative(self.output_layer_in)
+        # self.o1 = (self.y - self.output) * sigmoid_derivative(self.output)
+        self_hidden_unit = deque([])
+        for i in range(self.n_hidden_layer-1,-1,-1):
+            self_hidden_unit.appendleft(sigmoid_derivative(self.output_layer_in[i]) * )
+        # for i in range(self.n_hidden_layer):
+
+
+        #
+        # d_weights2 = np.dot(self.hidden_layer_out1.T, ((self.y - self.output) * sigmoid_derivative(self.output)))
+        # d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.hidden_layer_out1)))
+        # # weight_output = np.dot( ,(self.o1 = (self.y - self.output) * sigmoid_derivative(self.output)))
+        #
+        # # if (batch_size==1): #stochastic
+        # #
+        # # else:
+        #
+        # # update the weights with the derivative (slope) of the loss function
+        # self.weights1 += d_weights1
+        # self.weights2 += d_weights2
