0175ba442b6ee4c98a4038ff2b5b37bfc6a505b7
handikao29 <handikaoktavianus29@gmail.com>
2018-11-15 13:00:21 +0700

    calculation problem?

--- nn.py
+++ nn.py
@@ -31,24 +31,23 @@ class NeuralNetwork:
         self.momentum = momentum
         self.learning_rate = learning_rate
         self.weight_input = np.random.uniform(-.5,.5,[self.input.shape[1],self.hidden_layer[0]])
+        self.weight_input_prev = self.weight_input
         for i in range(len(self.hidden_layer)):
             if i > 0:
                 weight_layer_temp = np.random.uniform(-.5,.5,[self.hidden_layer[i-1],self.hidden_layer[i]])
                 self.weight_hidden_layer.append(weight_layer_temp)
 
+        self.weight_hidden_layer_prev = self.weight_hidden_layer
         self.weight_output = np.random.uniform(-.5,.5,[self.hidden_layer[-1],1])
-
+        self.weight_output_prev = self.weight_output
         for i in range(epoch):
             for j in range(len(self.input)):
-                print("j",j)
                 self.iterator = j
-                print(self.input[self.iterator])
                 self.feedforward()
                 self.backprop_search_gradient()
                 self.backprop_update_weight()
 
     def feedforward(self):
-        
         for i in range(self.n_hidden_layer):
             if i == 0:
                 self.layer.append(sigmoid(np.dot(self.input[self.iterator], self.weight_input)))
@@ -56,12 +55,10 @@ class NeuralNetwork:
                 self.layer.append(sigmoid(np.dot(self.layer[i-1], self.weight_hidden_layer[i-1])))
 
         output_layer = sigmoid(np.dot(self.layer[-1], self.weight_output))
-        print(output_layer)
-        print()
         self.output = output_layer
+        print(output_layer)
 
-    def backprop_search_gradient(self):
-        
+    def backprop_search_gradient(self):        
         for i in range(self.n_hidden_layer+2):
             if (i == 0):
                 self.delta_output = (self.y[self.iterator] - self.output) * sigmoid_derivative(self.output)
@@ -80,12 +77,11 @@ class NeuralNetwork:
                 # print(len(self.layer[self.n_hidden_layer-1]))
                 hidden_layer_unit = np.reshape(self.layer[self.n_hidden_layer-1], (len(self.layer[self.n_hidden_layer-1]),-1))
                 # print(hidden_layer_unit)
-                self.weight_output = self.weight_output + (self.momentum * np.multiply(np.transpose(self.delta_output), hidden_layer_unit))
-                # print(self.weight_output)
+                self.weight_output = self.weight_output + (self.momentum * np.multiply(np.transpose(self.delta_output), hidden_layer_unit)) + (self.learning_rate * self.weight_output_prev)
             elif i == self.n_hidden_layer :
                 # print(self.delta_hidden_layer[i-1])
                 hidden_layer_unit = np.reshape(self.input[self.iterator], (len(self.input[self.iterator]),-1))
-                self.weight_input = self.weight_input + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), hidden_layer_unit))
+                self.weight_input = self.weight_input + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), hidden_layer_unit)) + (self.learning_rate * self.weight_input_prev)
             else:
                 # print(i)
                 # print(self.weight_hidden_layer[self.n_hidden_layer-i-1])
@@ -93,4 +89,7 @@ class NeuralNetwork:
                 # print(np.transpose(self.layer[self.n_hidden_layer-i-1]))
                 # print(self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), np.transpose(self.layer[self.n_hidden_layer-i-1])))
                 hidden_layer_unit = np.reshape(self.layer[self.n_hidden_layer-i-1], (len(self.layer[self.n_hidden_layer-i-1]),-1))
-                self.weight_hidden_layer[self.n_hidden_layer-i-1] = self.weight_hidden_layer[self.n_hidden_layer-i-1] + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), hidden_layer_unit))
+                self.weight_hidden_layer[self.n_hidden_layer-i-1] = self.weight_hidden_layer[self.n_hidden_layer-i-1] + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), hidden_layer_unit)) +  + (self.learning_rate * self.weight_hidden_layer_prev[self.n_hidden_layer-i-1])
+        self.weight_input_prev = self.weight_input
+        self.weight_output_prev = self.weight_output
+        self.weight_hidden_layer_prev = self.weight_hidden_layer
