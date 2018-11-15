9c9c154bcd829254b395c86be23ab296c1d4f193
handikao29 <handikaoktavianus29@gmail.com>
2018-11-15 12:16:47 +0700

    Multi input done, learning rate + batch in progress

--- nn.py
+++ nn.py
@@ -17,8 +17,8 @@ class NeuralNetwork:
         self.n_hidden_layer = 0
         self.hidden_layer = []
         self.weight_hidden_layer = []
-
-
+        self.layer = []
+        self.delta_hidden_layer = []
 
     def add(self,unit):
         if self.n_hidden_layer < 10:
@@ -39,45 +39,58 @@ class NeuralNetwork:
         self.weight_output = np.random.uniform(-.5,.5,[self.hidden_layer[-1],1])
 
         for i in range(epoch):
-            self.feedforward()
-            self.backprop()
+            for j in range(len(self.input)):
+                print("j",j)
+                self.iterator = j
+                print(self.input[self.iterator])
+                self.feedforward()
+                self.backprop_search_gradient()
+                self.backprop_update_weight()
 
     def feedforward(self):
-        self.layer = []
+        
         for i in range(self.n_hidden_layer):
             if i == 0:
-                self.layer.append(sigmoid(np.dot(self.input, self.weight_input)))
+                self.layer.append(sigmoid(np.dot(self.input[self.iterator], self.weight_input)))
             else:
                 self.layer.append(sigmoid(np.dot(self.layer[i-1], self.weight_hidden_layer[i-1])))
 
         output_layer = sigmoid(np.dot(self.layer[-1], self.weight_output))
+        print(output_layer)
+        print()
         self.output = output_layer
-        print(self.output)
 
-    def backprop(self):
-
-        self.delta_hidden_layer = []
-        for i in range(self.n_hidden_layer+1):
+    def backprop_search_gradient(self):
+        
+        for i in range(self.n_hidden_layer+2):
             if (i == 0):
-                self.delta_output = (self.y - self.output) * sigmoid_derivative(self.output)
+                self.delta_output = (self.y[self.iterator] - self.output) * sigmoid_derivative(self.output)
             elif (i == 1):
                 self.delta_hidden_layer.append(np.dot(self.weight_output, self.delta_output) * sigmoid_derivative(self.output))
+            elif (i == self.n_hidden_layer+1):
+                self.delta_hidden_layer.append(np.dot(self.weight_input, self.delta_hidden_layer[i-2]) * sigmoid_derivative(self.output))
             else:
                 self.delta_hidden_layer.append(np.dot(self.weight_hidden_layer[self.n_hidden_layer-i], self.delta_hidden_layer[i-2]) * sigmoid_derivative(self.output))
 
-        for i in range(self.n_hidden_layer):
+    def backprop_update_weight(self):
+        for i in range(self.n_hidden_layer+1):
             if i == 0 :
-                self.weight_output = self.weight_output + (self.momentum * np.multiply(np.transpose(self.delta_output), np.transpose(self.layer[self.n_hidden_layer-1])))
-            elif i == self.n_hidden_layer-1 :
-                self.weight_input = self.weight_input + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i]), np.transpose(self.input)))
+                # print(self.delta_output)
+                # print(self.layer[self.n_hidden_layer-1])
+                # print(len(self.layer[self.n_hidden_layer-1]))
+                hidden_layer_unit = np.reshape(self.layer[self.n_hidden_layer-1], (len(self.layer[self.n_hidden_layer-1]),-1))
+                # print(hidden_layer_unit)
+                self.weight_output = self.weight_output + (self.momentum * np.multiply(np.transpose(self.delta_output), hidden_layer_unit))
+                # print(self.weight_output)
+            elif i == self.n_hidden_layer :
+                # print(self.delta_hidden_layer[i-1])
+                hidden_layer_unit = np.reshape(self.input[self.iterator], (len(self.input[self.iterator]),-1))
+                self.weight_input = self.weight_input + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), hidden_layer_unit))
             else:
-                self.weight_hidden_layer[self.n_hidden_layer-i-1] = self.weight_hidden_layer[self.n_hidden_layer-i-1] + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), np.transpose(self.layer[self.n_hidden_layer-i-1])))
-
-
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        # d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        # d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
-
-        # # update the weights with the derivative (slope) of the loss function
-        # self.weights1 += d_weights1
-        # self.weights2 += d_weights2
+                # print(i)
+                # print(self.weight_hidden_layer[self.n_hidden_layer-i-1])
+                # print(np.transpose(self.delta_hidden_layer[i-1]))
+                # print(np.transpose(self.layer[self.n_hidden_layer-i-1]))
+                # print(self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), np.transpose(self.layer[self.n_hidden_layer-i-1])))
+                hidden_layer_unit = np.reshape(self.layer[self.n_hidden_layer-i-1], (len(self.layer[self.n_hidden_layer-i-1]),-1))
+                self.weight_hidden_layer[self.n_hidden_layer-i-1] = self.weight_hidden_layer[self.n_hidden_layer-i-1] + (self.momentum * np.multiply(np.transpose(self.delta_hidden_layer[i-1]), hidden_layer_unit))
